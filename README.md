de95739df448181976e2f7406741e7184c8d9a21
datawrangl3r <sathyasarathi90@gmail.com>
2020-12-06 01:53:30 +0530

    fixed issues

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # logGenerator
+
 A python program to generate random log texts in regular intervals
 
 usage: generator.py [-h] -i ITERATIONS [-f FRAMEWORK] [-s SLEEP]
@@ -6,14 +7,21 @@ usage: generator.py [-h] -i ITERATIONS [-f FRAMEWORK] [-s SLEEP]
 Random Log Generator.
 
 optional arguments:
-  -h, --help            show this help message and exit
 
+```text
+  -h, --help            show this help message and exit
   -i ITERATIONS, --iterations ITERATIONS
                         Number of iterations
-
   -f FRAMEWORK, --framework FRAMEWORK
                         Name of framework
-
   -s SLEEP, --sleep SLEEP
                         Delay for the log to be printed
+```
 
+## Example
+
+```bash
+git clone https://github.com/datawrangl3r/logGenerator.git
+cd logGenerator
+python3 generator.py -i1000
+```
--- generator.py
+++ generator.py
@@ -2,24 +2,31 @@ import random
 import logging
 import time
 import argparse
-import ConfigParser
-from datetime import datetime 
+from configparser import ConfigParser
+from datetime import datetime
 
 parser = argparse.ArgumentParser(description='Random Log Generator.')
-parser.add_argument('-i', '--iterations', type=int, help='Number of iterations', required=True)
-parser.add_argument('-f', '--framework', type=str, default='lumen', help='Name of framework')
-parser.add_argument('-s', '--sleep', type=int, default=0, help='Delay for the log to be printed')
+parser.add_argument('-i', '--iterations',
+                    type=int,
+                    help='Number of iterations',
+                    required=True)
+parser.add_argument('-f', '--framework',
+                    type=str,
+                    default='lumen',
+                    help='Name of framework')
+parser.add_argument('-s', '--sleep',
+                    type=int,
+                    default=0,
+                    help='Delay for the log to be printed')
 args = parser.parse_args()
+config = ConfigParser()
+config.read('logging.cnf')
+msg = config.get(args.framework, "msg").split(",")
+logging.basicConfig(filename=f'{str(datetime.now().date())}.log',
+                    level=logging.DEBUG,
+                    format='%(message)s')
 
-config = ConfigParser.ConfigParser()
-config.readfp(open('loggenerator.conf'))
-msg = config.get(args.framework,"msg").split(',')
-
-logging.basicConfig(
-		filename='%s.log'%(str(datetime.now().date())), \
-		level=logging.DEBUG,\
-		format='%(message)s')
-
-for i in range (args.iterations):
-	logging.info('[%s] '%(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))+msg[random.randint(0, len(msg)-1)])
-	time.sleep(args.sleep)
+for i in range(args.iterations):
+    datestimetamp = str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    logging.info(f'{datestimetamp}'+msg[random.randint(0, len(msg)-1)])
+    time.sleep(args.sleep)
